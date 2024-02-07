30b3a1ed6b9cb62fd274c7f40d6dcf798c6c49ad
Kishan_Gaiwala <gaiwalakishan@gmail.com>
2024-02-07 14:17:02 +0530

    first commit

---
+++ src/data/info.ts
@@ -0,0 +1,92 @@
+export const info = {
+  baseUrl: "https://astro-portfolio-uzair.vercel.app",
+  name: "Kishan Gaiwala",
+  jobDescription: "Web Developer",
+  // about: `I'm a software engineer based in Karachi, Pakistan.
+  //  I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
+  //  I have been working for 3 year, with a demonstrated history of working in the Computer Software industry. Skilled in Front-end Development.
+  //  I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, React JS and Next JS. Hence,
+  //  I have successfully completed several web projects from scratch. 
+  //  Yet, I learn continuously and never stop exploring new technologies.
+  //  So, I have also gained knowledge of Node JS, Express JS, Mongo DB, Python,
+  //  firebase and React Native with time.
+  // `,
+  about: `I'm a web developer with 2 years of experience in creating websites and applications using HTML, CSS, JavaScript, and React. I know a bit about Node.js too. I love turning design ideas into user-friendly websites. I'm good at problem-solving and always eager to learn new things.
+  `,
+
+  experience: [
+    {
+      name: "Appgambit",
+      location: "Surat, India",
+      startDate: "Jun 2022",
+      endDate: "Present",
+      description: [
+        "- Developing screens and UI components for the web application using React and Tailwind.",
+        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
+      ],
+    },
+
+    {
+      name: "Logical Loop (Internship)",
+      location: "Surat, India",
+      startDate: "Dec 2021",
+      endDate: "Jun 2022",
+      description: [
+        "✓ Developing code in accordance with appropriate design and architectural rules",
+        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
+        "✓ Help and training of other colleagues",
+        "✓ Others tasks connected to the nature of the job",
+        "✓ Code Improvements and Refactoring ",
+        "✓ Best Practices and Rules Maintainer",
+      ],
+    },
+
+    {
+      name: "MNJ Tech (Internship)",
+      location: "Surat, India",
+      startDate: "Feb 2021",
+      endDate: "May 2021",
+      description: [
+        "An Immersive Internship Program that prepares me for the world's most in-demand career. A 3 months program completely based on learning and challenges.",
+        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Uka Tersadia University",
+      location: "Bardoli, Gujarat",
+      startDate: "2018",
+      endDate: "2022",
+      description: [
+        "Bachelor in Information & Technology",
+      ],
+    },
+  ],
+
+  socialMedia: {
+    // facebook: "https://www.facebook.com/profile.php?id=100022899849583",
+    // twitter: "https://twitter.com/Uzzii_21",
+    github: "https://github.com/Kishan-gaiwala",
+    email: "mailto:gaiwalakishan@gmail.com",
+    linkedin: "https://www.linkedin.com/in/kishan-gaywala-9414321b3/",
+  },
+
+  projects: [
+    {
+      title: "KindJob",
+      isFeatured: true,
+      thumbnail: "assets/p1.png",
+      // githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      liveUrl: "https://kindjobs.co/",
+    },
+    {
+      title: "Covest",
+      isFeatured: true,
+      thumbnail: "assets/p2.png",
+      githubUrl: "https://github.com/Kishan-gaiwala/CovetHtmlSite",
+      liveUrl: "https://covet-html-site.vercel.app/",
+    },
+  ],
+};
